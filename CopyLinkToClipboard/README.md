ec67a7c3a863b182c6bb362f7ac14f93bd72e368
Sankar Vema <sans.vema@gmail.com>
2020-01-20 10:04:08 +0530

    CopyLinkToClipboard: Append links to clipboard working

---
+++ CopyLinkToClipboard/README.md
@@ -0,0 +1,5 @@
+# Copy Link Text
+ Copy the text of the link.
+
+
+https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
---
+++ CopyLinkToClipboard/background.js
@@ -0,0 +1,62 @@
+"use strict";
+
+browser.menus.onClicked.addListener(async (info, tab) => {
+  if (info.menuItemId != "copy-link-text") {
+    return;
+  }
+
+  // let linkText = info.linkText;
+  let linkText = info.linkUrl;
+  if (info.modifiers &&
+      info.modifiers.length == 1 &&
+      info.modifiers[0] == "Shift") {
+    let result;
+    try {
+      result = await browser.tabs.executeScript(tab.id, {
+        frameId: info.frameId,
+        code: `
+          var title = "";
+          var elem = browser.menus.getTargetElement(${info.targetElementId});
+          while (elem) {
+            if (elem.href ||
+                elem.hasAttribute("href") ||
+                elem.hasAttributeNS("http://www.w3.org/1999/xlink", "href")) {
+              if (elem.hasAttribute("title")) {
+                title = elem.getAttribute("title");
+              }
+              break;
+            }
+            elem = elem.parentElement;
+          }
+          title;
+        `,
+      });
+    }
+    catch(ex) {
+      console.error(ex);
+    }
+    if (result &&
+        result[0] != "" &&
+        result[0] != linkText) {
+      linkText = result[0];
+    }
+  }
+
+  linkText = JSON.stringify(linkText)
+                 .replace(/^"|"$/g, "")
+                 .replace(/\\(?=")/g, "");
+
+   navigator.clipboard.readText().then(function(data) {
+     navigator.clipboard.writeText(data + '\r\n' + linkText).catch(() => {
+       console.error("Failed to copy the link text.");
+     });
+     // console.log(“Your string: ”, data);
+   });
+
+});
+
+browser.menus.create({
+  id: "copy-link-text",
+  title: browser.i18n.getMessage("contextMenuItemLink"),
+  contexts: ["link"],
+});
